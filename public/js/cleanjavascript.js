c4f03642792ee3405e9ca0dccbfdf6b3368a5181
Bakisyn <eugorx@gmail.com>
2021-12-15 01:49:39 +0100

    dodao drugaciji scroll za contact

--- public/js/cleanjavascript.js
+++ public/js/cleanjavascript.js
@@ -181,7 +181,7 @@ for (var i = 0; i < elements.length; i++) {
         if(timeBarWidth < 768){
           animate(document.scrollingElement || document.documentElement, "scrollTop", "", 0, target.offsetTop - 75, 2000, true);
         }else{
-          animate(document.scrollingElement || document.documentElement, "scrollTop", "", 0, target.offsetTop - 250, 2000, true);
+          animate(document.scrollingElement || document.documentElement, "scrollTop", "", 0, target.offsetTop - 250, 2000, true); 
         }
         document.getElementById("entered-form-name").focus();
       }
